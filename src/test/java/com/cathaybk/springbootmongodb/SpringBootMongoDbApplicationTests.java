a24ce1da82a30082c483ddbd2eccfd965d5805b3
Roger <sobadrush@icloud.com>
2022-06-16 09:12:49 +0800

    [測試] SpringBoot 單元測試

--- src/test/java/com/cathaybk/springbootmongodb/SpringBootMongoDbApplicationTests.java
+++ src/test/java/com/cathaybk/springbootmongodb/SpringBootMongoDbApplicationTests.java
@@ -1,13 +1,21 @@
 package com.cathaybk.springbootmongodb;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
-@SpringBootTest
+@SpringBootTest(classes = { SpringBootMongoDbApplication.class })
 class SpringBootMongoDbApplicationTests {
 
+    @Autowired
+    private Environment environment;
+
     @Test
+    @Disabled
     void contextLoads() {
+        System.err.println("environment = " + environment.getProperty("server.servlet.context-path"));
     }
 
 }
